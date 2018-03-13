aaf77b8986cdadd8e0d3db5b8bcdbb40ac8d73a1
Hugo Ricardo Silva <hugo_silva@artear.com>
2018-03-13 13:20:09 -0300

    Add example with react-redux

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import App from './containers/App2';
+//import App from './containers/App';
 
-// main app
-import App from './containers/App';
+import store from "./store/index";
+//import { addArticle } from "./actions/index";
 
-ReactDOM.render(<App />, document.getElementById('app'))
+//window.store = store;
+//window.addArticle = addArticle;
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'))
